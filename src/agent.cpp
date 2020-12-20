6c699757d61525e1108f26e7a1db651b8929d716
mengwei <rnwei0.0@gmail.com>
2020-12-20 15:11:21 +0800

    add planner and agent nodes

---
+++ src/agent.cpp
@@ -0,0 +1,64 @@
+#include "ros/ros.h"
+#include "planner_agent/get_plan.h"
+#include "planner_agent/update_goal.h"
+#include "planner_agent/agent_feedback.h"
+#include <cstdlib>
+
+ros::ServiceClient client;
+planner_agent::location current_location;
+long serial_id;
+
+bool update_goal(planner_agent::update_goal::Request   &rq,
+                  planner_agent::update_goal::Response  &res){
+  planner_agent::get_plan srv;
+  srv.request.goal = rq.goal;
+  srv.request.serial_id = serial_id;
+  if(client.call(srv)) {
+    current_location.x = rq.goal.x;
+    current_location.y = rq.goal.y;
+    res.result = 1;
+    return true;
+  } else {
+    res.result = 0;
+    return false;
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "agent");
+  if (argc != 4)
+  {
+    ROS_INFO("usage: get_plan serial_id current_position_x current_position_y");
+    return 1;
+  }
+
+  serial_id = atoll(argv[1]);
+  long current_position_x = atoll(argv[2]);
+  long current_position_y = atoll(argv[3]);
+  current_location.x = current_position_x;current_location.y = current_position_y;
+
+  ros::NodeHandle n;
+  ros::Publisher feedback = n.advertise<planner_agent::agent_feedback>("agent_feedback", 1000);
+  ros::ServiceServer service = n.advertiseService("update_goal", update_goal);
+  client = n.serviceClient<planner_agent::get_plan>("get_plan");
+
+  ros::Rate loop_rate(10);
+
+  while (ros::ok())
+  {
+    planner_agent::agent_feedback msg;
+
+    msg.serial_id = serial_id;
+    msg.current_location.x = current_location.x;
+    msg.current_location.y = current_location.y;
+
+    feedback.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
---
+++ src/planner.cpp
@@ -0,0 +1,95 @@
+#include "ros/ros.h"
+#include "planner_agent/location.h"
+#include "planner_agent/get_plan.h"
+#include "planner_agent/agent_feedback.h"
+#include "pathfinding.h"
+
+#include <sstream>
+
+struct Agent {
+    long serialID, x, y;
+
+    Agent(): serialID(0), x(0), y(0) {}
+
+    Agent(long serialID_, long x_, long y_): serialID(serialID_), x(x_), y(y_) {}
+};
+
+namespace std {
+    template <> struct hash<Agent> {
+        typedef Agent argument_type;
+        typedef std::size_t result_type;
+        std::size_t operator()(const Agent& agent) const noexcept {
+            return std::hash<int>()((agent.serialID << 1) ^ (agent.serialID >> 1));
+        }
+    };
+}
+
+std::unordered_map<long, Agent> agents;
+
+bool positionChanged(long oldX, long oldY, long newX, long newY) {
+    return (oldX != newX) || (oldY != newY);
+}
+
+void agent_feedback_callback(const planner_agent::agent_feedback::ConstPtr& msg)
+{
+  long serial_id = msg->serial_id;
+  long x = msg->current_location.x;
+  long y = msg->current_location.y;
+  ROS_INFO("agent feedback %ld at %ld %ld", serial_id, x, y);
+  if(agents.find(serial_id) == agents.end()) {
+      agents[serial_id] = Agent{serial_id, x, y};
+  } else if(positionChanged(1L, 1L, 2L, 1L)){
+      agents[serial_id] = Agent{serial_id, x, y};
+  }
+}
+
+bool generate_plan(planner_agent::get_plan::Request   &rq,
+                   planner_agent::get_plan::Response  &res)
+{
+  std::vector<GridLocation> path;
+
+  long serial_id = rq.serial_id;
+  long x = rq.goal.x;
+  long y = rq.goal.y;
+
+  ROS_INFO("generating plan for %ld at %ld %ld", serial_id, x, y);
+
+  if(agents.find(serial_id) == agents.end()) {
+      ROS_INFO("requesting agent has never feedback, skipping");
+      return false;
+  }
+
+  long current_x = agents[serial_id].x;
+  long current_y = agents[serial_id].y;
+
+  if(!positionChanged(x, y, current_x, current_y)) {
+      ROS_INFO("requesting goal equals to agent's current position, skipping");
+      return false;
+  }
+
+  ROS_INFO("requesting goal differ to current position %ld %ld, processing", current_x, current_y);
+
+  GridLocation start{current_x, current_y}, goal{x, y};
+  path = searchOnDefaultGraph(start, goal);
+  for(int i = 0; i < path.size(); i++) {
+      planner_agent::location l;
+      l.x = path[i].x;l.y = path[i].y;
+      res.path.push_back(l);
+  }
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "planner");
+
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("agent_feedback", 1000, agent_feedback_callback);
+  ros::ServiceServer service = n.advertiseService("get_plan", generate_plan);
+  ROS_INFO("Ready to generate_plan.");
+
+  ros::spin();
+
+  return 0;
+}
